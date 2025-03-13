5c65be6db5b4f458e5d2044bda2680979037b27a
Christopher Leu <cl3880@nyu.edu>
2025-03-13 07:57:09 -0400

    Sourced quotes and created data collection. Implemented get() function with num_quotes parameter. Updated main.py and .gitignore

---
+++ src/procrationationhelper/__main__.py
@@ -0,0 +1,32 @@
+"""
+In Python packages, this file called __main__.py is run when the package is run
+directly from command line, as opposed to importing it into another program.
+"""
+
+import argparse
+import procrationationhelper.quotes as quotes
+
+
+def main():
+    """
+    Get procrastination quotes and print them out.
+    """
+    parser = argparse.ArgumentParser(description="Get procrastination quotes")
+    parser.add_argument(
+        "-n", "--number",
+        type=int,
+        default=1,
+        help="Number of quotes to display (default: 1)"
+    )
+    args = parser.parse_args()
+    
+    result = quotes.get(num_quotes=args.number)
+    
+    print("\nProcrastination Wisdom:")
+    print(result)
+    print("\nNote: More features coming soon!")
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ src/procrationationhelper/data/quotesData.py
@@ -0,0 +1,106 @@
+"""
+Collection of procrastination quotes for the procrastination_helper package.
+
+Source:
+- Goodreads: https://www.goodreads.com/quotes/tag/procrastination
+"""
+
+# Quotes data - collection of procrastination quotes with authors
+quotes = [
+    {
+        "text": "The best way to get something done is to begin.",
+        "author": "Unknown"
+    },
+    {
+        "text": "You can't just turn on creativity like a faucet. You have to be in the right mood. What mood is that? Last-minute panic.",
+        "author": "Bill Watterson"
+    },
+    {
+        "text": "Only put off until tomorrow what you are willing to die having left undone.",
+        "author": "Pablo Picasso"
+    },
+    {
+        "text": "I can't think about that right now. If I do, I'll go crazy. I'll think about that tomorrow.",
+        "author": "Margaret Mitchell"
+    },
+    {
+        "text": "Procrastinate now, don't put it off.",
+        "author": "Ellen DeGeneres"
+    },
+    {
+        "text": "I never put off till tomorrow what I can possibly do - the day after.",
+        "author": "Oscar Wilde"
+    },
+    {
+        "text": "Time is an equal opportunity employer. Each human being has exactly the same number of hours and minutes every day. Rich people can't buy more hours. Scientists can't invent new minutes.",
+        "author": "Denis Waitley"
+    },
+    {
+        "text": "You cannot escape the responsibility of tomorrow by evading it today.",
+        "author": "Abraham Lincoln"
+    },
+    {
+        "text": "It is easier to resist at the beginning than at the end.",
+        "author": "Leonardo da Vinci"
+    },
+    {
+        "text": "If it weren't for the last minute, nothing would get done.",
+        "author": "Rita Mae Brown"
+    },
+    {
+        "text": "My advice is, never do to-morrow what you can do today. Procrastination is the thief of time. Collar him!",
+        "author": "Charles Dickens"
+    },
+    {
+        "text": "God has promised forgiveness to your repentance, but He has not promised tomorrow to your procrastination.",
+        "author": "St. Augustine of Hippo"
+    },
+    {
+        "text": "You can dance in the storm. Don't wait for the rain to be over because it might take too long. You can do it now.",
+        "author": "Israelmore Ayivor"
+    },
+    {
+        "text": "Procrastination is my sin. It brings me naught but sorrow. I know that I should stop it. In fact, I will--tomorrow.",
+        "author": "Gloria Pitzer"
+    },
+    {
+        "text": "We are so scared of being judged that we look for every excuse to procrastinate.",
+        "author": "Erica Jong"
+    },
+    {
+        "text": "The scholar's greatest weakness: calling procrastination research.",
+        "author": "Stephen King"
+    },
+    {
+        "text": "As a writer, I need an enormous amount of time alone. Writing is 90 percent procrastination: reading magazines, eating cereal out of the box, watching infomercials.",
+        "author": "Paul Rudnick"
+    },
+    {
+        "text": "Think of your many years of procrastination; how the gods have repeatedly granted you further periods of grace, of which you have taken no advantage.",
+        "author": "Marcus Aurelius"
+    },
+    {
+        "text": "My mother always told me I wouldn't amount to anything because I procrastinate. I said, 'Just wait.'",
+        "author": "Judy Tenuta"
+    },
+    {
+        "text": "It is awfully hard work doing nothing. However, I don't mind hard work where there is no definite object of any kind.",
+        "author": "Oscar Wilde"
+    },
+    {
+        "text": "Never put off for tomorrow, what you can do today.",
+        "author": "Aaron Burr"
+    },
+    {
+        "text": "Plan to be spontaneous tomorrow.",
+        "author": "Steven Wright"
+    },
+    {
+        "text": "Success is not obtained overnight. It comes in installments; you get a little bit today, a little bit tomorrow until the whole package is given out.",
+        "author": "Israelmore Ayivor"
+    },
+    {
+        "text": "Never put off till tomorrow the book you can read today.",
+        "author": "Holbrook Jackson"
+    }
+]
